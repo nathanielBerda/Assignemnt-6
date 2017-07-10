b3eeb6c6d5406feda5db45329a55f9a9c589d81e
nathanielBerda <nathanielberda@hotmail.com>
2017-07-10 15:20:57 +0300

    Land done
    Monday

--- js/javascript.js
+++ js/javascript.js
@@ -5,23 +5,72 @@ main.style.zIndex = 9;
 main.style.border = "2px solid";
 main.style.marginLeft= "20px";
 main.style.marginTop= "20px";
-main.style.background= "#7FFFD4";
+main.style.verticalAlign="bottom";
 var body = document.getElementsByTagName("body")[0];
 body.appendChild(main);
 
 body.style.margin="0"
 
 
+array= new Array(20);
 
-for(i=0;i<10;i++){
-    var dirt = document.createElement('div');
-dirt.className="dirt"
-
-    main.appendChild(dirt);
+for(i=0;i<20;i++){
+    array[i]=Math.floor(Math.random()*3+2);
+    console.log(array[i]);
 }
 
-
-array= new Array(20);
 for(i=0;i<20;i++){
-    array[i]=Math.floor(Math.random());
+    for(j=0;j<array[i];j++){
+    var dirt = document.createElement('div');
+    dirt.setAttribute("x", i);
+    dirt.setAttribute('y', j);
+    dirt.id="d"+"x"+i+"y"+j;
+    dirt.className="square dirt";
+    dirt.style.marginLeft= i*50 + "px";
+    dirt.style.marginTop= 550-j*50+ "px";
+    dirt.innerHTML= i+","+j;
+    main.appendChild(dirt);
+    }
+    var grass = document.createElement('div');
+    grass.setAttribute("x", i);
+    grass.id="g"+"x"+i+"y"+j;
+    grass.setAttribute('y', array[i]);
+    grass.className="square grass";
+    grass.style.marginLeft= i*50 + "px";
+    grass.style.marginTop= 550-array[i]*50+ "px";
+    grass.innerHTML= i+","+array[i];
+    main.appendChild(grass);
+    for(j=array[i]+1;j<12;j++){
+    var sky = document.createElement('div');
+    sky.setAttribute("x", i);
+    sky.id="s"+"x"+i+"y"+j;
+    sky.setAttribute('y', j);
+    sky.className="square sky"
+    sky.style.marginLeft= i*50 + "px";
+    sky.style.marginTop= 550-j*50+ "px";
+    sky.innerHTML= i+","+j;
+    main.appendChild(sky);
+    }
+}
+
+t=Math.floor(Math.random()*16+2);
+for(i=0;i<3;i++){
+var tree = document.createElement('div');
+    tree.className="square tree";
+    tree.style.marginLeft= t*50 + "px";
+    tree.style.marginTop= 550-array[t]*50-50*(i+1)+"px";
+    tree.innerHTML= i+","+array[i];
+    tree.id="t"+"x"+i+"y"+j;
+    main.appendChild(tree);
+}
+for(i=0;i<3;i++){
+    for(j=0;j<4;j++){
+    var leaf = document.createElement('div');
+    leaf.className="square leaf";
+    leaf.style.marginLeft= t*50-100+i*50+50 + "px";
+    leaf.style.marginTop= 550-array[t]*50-4*50-50*j+"px";
+    leaf.innerHTML= i+","+array[i];
+    leaf.id="t"+"x"+i+"y"+j;
+    main.appendChild(leaf);
+    }
 }
